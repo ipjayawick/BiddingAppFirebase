28e480dc2822b0f456559a7152a8c9315a608bd0
ipjayawick <ipjayawick@gmail.com>
2024-06-13 17:24:38 +0530

    bidding info card updated

--- client/src/components/BiddingInfoCard.jsx
+++ client/src/components/BiddingInfoCard.jsx
@@ -8,7 +8,7 @@ import { AuthContext } from '../context/AuthContext';
 import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
 import { Button, List, ListItem, ListItemText } from '@mui/material';
-import { doc, getDoc, onSnapshot, setDoc, updateDoc } from 'firebase/firestore';
+import { arrayRemove, deleteField, doc, getDoc, increment, onSnapshot, setDoc, updateDoc } from 'firebase/firestore';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { db } from '../config/firebase';
@@ -16,6 +16,8 @@ import { db } from '../config/firebase';
 export default function OutlinedCard() {
     const [activeCompany, setActiveCompany] = useState(null)
     const [isBiddingActive, setIsBiddingActive] = useState(false);
+    const [activeCompanyId, setActiveCompanyId] = useState(null)
+    const [updateCount, setUpdateCount] = useState(0);
 
     useEffect(() => {
         const unsubscribe = onSnapshot(doc(db, "controlData", "activeCompany"), async (doc) => {
@@ -23,13 +25,14 @@ export default function OutlinedCard() {
             if (companyRef) {
                 const data = await getDoc(companyRef)
                 setActiveCompany(data.data())
+                setActiveCompanyId(doc.data().activeCompanyId)
             } else {
                 setActiveCompany(null)
             }
             setIsBiddingActive(doc.data().isBiddingActive)
         });
         return () => unsubscribe();
-    }, []);
+    }, [updateCount]);
 
     const updateteBiddingStatus = async (isBiddingActive) => {
         await updateDoc(doc(db, "controlData", "activeCompany"), {
@@ -37,51 +40,81 @@ export default function OutlinedCard() {
         })
     };
 
+    const removeBidderFromCompany = async (userId) => {
+        try {
+            await updateDoc(doc(db, "companies", activeCompanyId), {
+                [`bidders.${userId}`]: deleteField()
+            })
+            await updateDoc(doc(db, 'users', userId), {
+                companies: arrayRemove(activeCompany.companyName),
+                remainingBiddingPoints: increment(activeCompany.biddingMargin)
+            })
+            setUpdateCount(prev => prev + 1);
+        } catch (error) {
+            console.error('Error deleting bidder from company:', error)
+        }
+    }
+
     if (activeCompany === null) {
         return (
             <>
-                Loading...
+                <Typography variant="h4" mb={2.5}>Active Company</Typography>
+                Select a Company
             </>
         )
     }
 
     return (
         <>
+            <Typography variant="h4" mb={2.5}>Active Company</Typography>
             <Card variant="outlined" sx={{ maxWidth: 360 }}>
                 <Box sx={{ p: 2, pb: 0 }}>
                     <Stack direction="row" justifyContent="space-between" alignItems="center" >
-                        <Typography variant="h6" component="div">
+                        <Typography fontSize={18} component="div">
                             {activeCompany?.companyName}
                         </Typography>
-                        <Button variant="contained" color="primary">Change Margin</Button>
+
                         <Button
                             variant="contained"
                             color={isBiddingActive ? "error" : "success"}
                             onClick={() => updateteBiddingStatus(!isBiddingActive)}
                         >
-                            {isBiddingActive ? "Stop" : "Start"}
+                            {isBiddingActive ? "Stop Bidding" : "Start Bidding"}
                         </Button>
                     </Stack>
                 </Box>
                 <Divider sx={{ mt: 2 }} />
                 <>
-                    <Box sx={{ p: 2, pb: 0 }}>
+                    <Box sx={{ p: 2 }}>
                         <Stack direction="row" justifyContent="space-between" alignItems="center">
-                            <Typography gutterBottom variant="h6" component="div">
+                            <Typography gutterBottom fontSize={18} component="div" mb={0}>
                                 Bidding Margin
                             </Typography>
-                            <Typography gutterBottom variant="h6" component="div">
+                            <Button variant="contained" color="primary" sx={{ marginLeft: "auto", marginRight: 2 }}>Change</Button>
+                            <Typography gutterBottom fontSize={18} component="div" mb={0}>
                                 {activeCompany?.biddingMargin}
                             </Typography>
                         </Stack>
                     </Box>
                     <Divider />
-                    <Box sx={{ p: 2, pt: 0 }}>
+                    <Box sx={{ p: 2, backgroundColor: (activeCompany.totalVacancies < Object.keys(activeCompany.bidders).length) && "#FF6865" }}>
                         <Stack direction="row" justifyContent="space-between" alignItems="center">
-                            <Typography gutterBottom variant="h6" component="div" sx={{ mb: 0 }}>
+                            <Typography gutterBottom fontSize={18} component="div" mb={0} >
+                                Vacancies for Round
+                            </Typography>
+                            <Button variant="contained" color="primary" sx={{ marginLeft: "auto", marginRight: 2 }}>Change</Button>
+                            <Typography gutterBottom fontSize={18} component="div" mb={0}>
+                                {activeCompany?.totalVacancies}
+                            </Typography>
+                        </Stack>
+                    </Box>
+                    <Divider />
+                    <Box sx={{ p: 2 }}>
+                        <Stack direction="row" justifyContent="space-between" alignItems="center">
+                            <Typography gutterBottom fontSize={18} component="div" >
                                 Bidders
                             </Typography>
-                            <Typography gutterBottom variant="h6" component="div">
+                            <Typography gutterBottom fontSize={18} component="div">
                                 {activeCompany?.bidders ? Object.keys(activeCompany.bidders).length : null}
                             </Typography>
                         </Stack>
@@ -91,6 +124,7 @@ export default function OutlinedCard() {
                                     <React.Fragment key={index}>
                                         <ListItem sx={{ py: 0, pt: 1 }}>
                                             <ListItemText primary={activeCompany.bidders[bidder].userName} />
+                                            <Button variant="contained" color="error" size='small' onClick={() => removeBidderFromCompany(activeCompany.bidders[bidder].userId)}>Remove</Button>
                                         </ListItem>
                                         <Divider variant="middle" component="li" />
                                     </React.Fragment>
